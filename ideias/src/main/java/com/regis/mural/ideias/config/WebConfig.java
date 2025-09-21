dd842beade119680edbae379729f96b16f3f951d
RegisMichael25 <regis.michael101@gmail.com>
2025-09-21 12:26:12 -0300

    Ajuste de CORS

---
+++ ideias/src/main/java/com/regis/mural/ideias/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.regis.mural.ideias.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite CORS para todos os endpoints
+                .allowedOrigins("http://localhost:5173") // A URL do seu frontend Vite
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
